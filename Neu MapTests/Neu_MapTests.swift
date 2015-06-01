78c81ebce2b323fe31ee8a44540c72d7a146888a
MacPro <MacPro@MacPros-Mac-Pro.local>
2015-06-01 14:51:18 +0700

    Initial Commit

---
+++ Neu MapTests/Neu_MapTests.swift
@@ -0,0 +1,36 @@
+//
+//  Neu_MapTests.swift
+//  Neu MapTests
+//
+//  Created by MacPro on 6/1/2558 BE.
+//  Copyright (c) 2558 EWTC Institute. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Neu_MapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
